aa9eee900ebe5636ca073619a4fd858183395b19
nilsbuehrer <nils@ception.ch>
2019-12-17 21:04:07 +0100

    Updates

---
+++ precache-manifest.9b1f667830a26433f06a546c7c1cc2c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19aff99f99868ee069ff0cea4c80d730",
+    "url": "/guesstimate/index.html"
+  },
+  {
+    "revision": "1031a0e22094dc82e567",
+    "url": "/guesstimate/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4f0a00071f2b0ddbb3f6",
+    "url": "/guesstimate/static/js/2.47d09da9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/guesstimate/static/js/2.47d09da9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1031a0e22094dc82e567",
+    "url": "/guesstimate/static/js/main.07ec982a.chunk.js"
+  },
+  {
+    "revision": "dcfe9bc142eb187aa7ce",
+    "url": "/guesstimate/static/js/runtime-main.6f229257.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/guesstimate/static/media/logo.5d5d9eef.svg"
+  }
+]);
