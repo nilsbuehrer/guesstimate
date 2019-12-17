4459491a6f871d2dcfaa65b7dcb520b38be94966
nilsbuehrer <nils@ception.ch>
2019-12-17 21:46:03 +0100

    init design

---
+++ src/Question.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Question.css';
+
+function Question() {
+    return (
+        <span className="Question-coming-soon">Coming soon.</span>
+    );
+}
+
+export default Question;
