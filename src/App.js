bbdabf1f4a06873a52535f578222cbca38b746df
nilsbuehrer <nils@ception.ch>
2020-02-05 21:37:08 +0100

    version

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import QuestionForm from "./QuestionForm";
+import packageJson from '../package.json';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <h1 className="App-title">Guesstimate</h1>
         <QuestionForm />
       </div>
+      <span class="version">v.{packageJson.version}</span>
     </div>
   );
 }
